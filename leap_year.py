868b58bd4cd0b76cc86e40d86ad1ceb5105a5514
Gustav Ihlenfeld <gustav.ihlenfeld@stu.ocsb.ca>
2025-03-31 21:38:18 +0000

    Final changes to style

--- leap_year.py
+++ leap_year.py
@@ -9,21 +9,19 @@ def main():
     try:
         user_year = int(input("Enter a year: "))
         # Nested if statement seeing if year is divisible by 4, 100, 400.
-         if user_year % 4 == 0:
-             if user_year % 100 == 0:
-                 if user_year % 400 == 0:
-                     print(f"{user_year} is a leap year.")
-                 else:
-                     print(f"{user_year} is not a leap year.")
-             else:
-                 print(f"{user_year} is a leap year.")
-         else:
-             print(f"{user_year} is not a leap year.")
+        if user_year % 4 == 0:
+            if user_year % 100 == 0:
+                if user_year % 400 == 0:
+                    print(f"{user_year} is a leap year.")
+                else:
+                    print(f"{user_year} is not a leap year.")
+            else:
+                print(f"{user_year} is a leap year.")
+        else:
+            print(f"{user_year} is not a leap year.")
     # Try catch making sure program doesn't crash
-     except ValueError:
-         print("Invalid input. Please enter a valid year.")
-
-    # Calling the function
+    except ValueError:
+        print("Invalid input. Please enter a valid year.")
 
 
 if __name__ == "__main__":
