6c344333281ff036c7ac1cee31ac32174a40cbc7
Gustav Ihlenfeld <gustav.ihlenfeld@stu.ocsb.ca>
2025-03-31 21:36:36 +0000

    Style changes

--- leap_year.py
+++ leap_year.py
@@ -8,21 +8,23 @@ def main():
     # Gets year input
     try:
         user_year = int(input("Enter a year: "))
-    #Nested if statement seeing if year is divisible by 4, 100, 400.
-        if user_year % 4 == 0:
-            if user_year % 100 == 0:
-                if user_year % 400 == 0:
-                    print(f"{user_year} is a leap year.")
-                else:
-                    print(f"{user_year} is not a leap year.")
-            else:
-                print(f"{user_year} is a leap year.")
-        else:
-            print(f"{user_year} is not a leap year.")
-    #Try catch making sure program doesn't crash
-    except ValueError:
-        print("Invalid input. Please enter a valid year.")
+        # Nested if statement seeing if year is divisible by 4, 100, 400.
+         if user_year % 4 == 0:
+             if user_year % 100 == 0:
+                 if user_year % 400 == 0:
+                     print(f"{user_year} is a leap year.")
+                 else:
+                     print(f"{user_year} is not a leap year.")
+             else:
+                 print(f"{user_year} is a leap year.")
+         else:
+             print(f"{user_year} is not a leap year.")
+    # Try catch making sure program doesn't crash
+     except ValueError:
+         print("Invalid input. Please enter a valid year.")
 
-    #Calling the function
+    # Calling the function
+
+
 if __name__ == "__main__":
     main()
